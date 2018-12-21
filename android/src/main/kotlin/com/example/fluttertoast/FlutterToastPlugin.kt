db5aea741345f2eddacfcbd1d1920c75b2f5bb3e
cuongtruong <cuongtruong@teqnological.asia>
2018-12-21 16:43:28 +0700

    Initial commit

---
+++ android/src/main/kotlin/com/example/fluttertoast/FlutterToastPlugin.kt
@@ -0,0 +1,27 @@
+package com.example.fluttertoast
+
+import android.widget.Toast
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry.Registrar
+
+class FlutterToastPlugin(private val registrar: Registrar): MethodCallHandler {
+  companion object {
+    @JvmStatic
+    fun registerWith(registrar: Registrar) {
+      val channel = MethodChannel(registrar.messenger(), "flutter_toast")
+      channel.setMethodCallHandler(FlutterToastPlugin())
+    }
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    if (call.method.equals("showToast")) {
+      val message: String = call.argument("message")
+      Toast.makeText(registrar.context(), message, Toast.LENGTH_LONG).show()
+    } else {
+      result.notImplemented()
+    }
+  }
+}
